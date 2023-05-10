dcf8fb287a40da32dc1f37516f7d2eda1d20c601
vaishnavimandage08 <vaishnavimandage08@gmail.com>
2023-05-10 11:25:06 -0700

    Added app file

---
+++ calc.py
@@ -0,0 +1,18 @@
+import tkinter as tk
+
+class Calculator:
+    def __init__(self):
+        self.window = tk.Tk()
+        self.window.title("Calculator")
+
+        self.entry = tk.Entry(self.window, width=25, font=("Arial", 16))
+        self.entry.grid(row=0, column=0, columnspan=4)
+
+       
+
+    def run(self):
+        self.window.mainloop()
+
+if __name__ == "__main__":
+    calc = Calculator()
+    calc.run()
